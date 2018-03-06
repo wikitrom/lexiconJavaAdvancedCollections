b1dd37ef5b744890c6fce0f3b21ff2c08f9896ce
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-06 10:21:41 +0100

    Sort objects by implementing the Comparable Interface

--- src/sortingObjects/Book.java
+++ src/sortingObjects/Book.java
@@ -1,11 +1,38 @@
 package sortingObjects;
 
-public class Book {
-	
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.TitlePaneLayout;
+
+public class Book implements Comparable<Book> {
+
 	private int id;
 	private String title;
 	private String author;
-	
+
+	// @Override
+	// public int compareTo(Book book) { // compare id field
+	// return this.id - book.id;
+	// }
+
+	// @Override
+	// public int compareTo(Book book) { // compare id field, reverse order
+	// return book.getId() - this.id;
+	// }
+
+//	@Override
+//	public int compareTo(Book book) { // compare title field alphabetically
+//		return this.title.compareTo(book.getTitle()); // use String compareTo method
+//	}
+
+	@Override
+	public int compareTo(Book book) { // compare title field alphabetically, then by author
+		int titleResult;
+		 titleResult = this.title.compareTo(book.getTitle()); 
+		 if (titleResult == 0) {   // same title, compare author
+			 return this.author.compareTo(book.getAuthor());
+		 }
+		 return titleResult;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -13,7 +40,7 @@ public class Book {
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public String getAuthor() {
 		return author;
 	}
@@ -23,9 +50,9 @@ public class Book {
 		this.title = title;
 		this.author = author;
 	}
-	
+
 	public String toString() {
 		return title + " by " + author;
 	}
-		
+
 }
--- src/sortingObjects/Main.java
+++ src/sortingObjects/Main.java
@@ -1,4 +1,5 @@
 package sortingObjects;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -6,30 +7,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public class Main {
-	
+
 	public static void main(String[] args) {
 		List<Book> books = new ArrayList<Book>();
-		books.add(new Book(1,"The Lord of the Rings","J. R. R. Tolkien"));
-		books.add(new Book(2,"Le Petit Prince","Antoine de Saint-Exupery"));
-		books.add(new Book(3,"Harry Potter and the Philosopher's Stone","J. K. Rowling"));
-		books.add(new Book(4,"And Then There Were None","Agatha Christie"));
-		books.add(new Book(5,"Dream of the Red Chamber","Cao Xueqin"));
-		books.add(new Book(6,"The Hobbit","J. R. R. Tolkien"));
-		books.add(new Book(7,"She: A History of Adventure","H. Rider Haggard"));
-		books.add(new Book(8,"The Lion, the Witch and the Wardrobe","C. S. Lewis"));
-		books.add(new Book(9,"The Da Vinci Code","Dan Brown"));
-		books.add(new Book(10,"The Catcher in the Rye","J. D. Salinger"));
+		books.add(new Book(1, "The Lord of the Rings", "J. R. R. Tolkien"));
+		books.add(new Book(2, "Le Petit Prince", "Antoine de Saint-Exupery"));
+		books.add(new Book(3, "Harry Potter and the Philosopher's Stone", "J. K. Rowling"));
+		books.add(new Book(4, "And Then There Were None", "Agatha Christie"));
+		books.add(new Book(5, "Dream of the Red Chamber", "Cao Xueqin"));
+		books.add(new Book(6, "The Hobbit", "J. R. R. Tolkien"));
+		books.add(new Book(7, "She: A History of Adventure", "H. Rider Haggard"));
+		books.add(new Book(8, "The Lion, the Witch and the Wardrobe", "C. S. Lewis"));
+		books.add(new Book(9, "The Da Vinci Code", "Dan Brown"));
+		books.add(new Book(10, "The Catcher in the Rye", "J. D. Salinger"));
+		books.add(new Book(11, "The Hobbit", "AB Svenska Ord"));
 
-		
+		System.out.println();
+		System.out.println("Unsorted Books, as stored in ArrayList.");
+		System.out.println("=========================================");
 		for (Book nextBook : books) {
 			System.out.println(nextBook);
 		}
+
+		System.out.println();
+		System.out.println("Sorted books");
+		System.out.println("=================================================================");
+
+		Collections.sort(books);   // uncomment suitable compareTo() in Book.java
+
+		for (Book nextBook : books) {
+			System.out.println(nextBook);
+		}
+
 	}
-
-	
-
-	
 
 }
