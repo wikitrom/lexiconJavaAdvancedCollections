ccdf5b98df88fb4a8015ce71b831463005040e53
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-06 13:50:07 +0100

    Use HashMap to store data

--- src/sortingObjects/Main.java
+++ src/sortingObjects/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,61 +13,25 @@ import performance.TitleSort;
 public class Main {
 
 	public static void main(String[] args) {
-		List<Book> books = new ArrayList<Book>();
-		books.add(new Book(1, "The Lord of the Rings", "J. R. R. Tolkien"));
-		books.add(new Book(2, "Le Petit Prince", "Antoine de Saint-Exupery"));
-		books.add(new Book(3, "Harry Potter and the Philosopher's Stone", "J. K. Rowling"));
-		books.add(new Book(4, "And Then There Were None", "Agatha Christie"));
-		books.add(new Book(5, "Dream of the Red Chamber", "Cao Xueqin"));
-		books.add(new Book(6, "The Hobbit", "J. R. R. Tolkien"));
-		books.add(new Book(7, "She: A History of Adventure", "H. Rider Haggard"));
-		books.add(new Book(8, "The Lion, the Witch and the Wardrobe", "C. S. Lewis"));
-		books.add(new Book(9, "The Da Vinci Code", "Dan Brown"));
-		books.add(new Book(10, "The Catcher in the Rye", "J. D. Salinger"));
-		books.add(new Book(11, "The Hobbit", "AB Svenska Ord"));
+		Map<Book, Integer> books = new HashMap();
+		books.put(new Book(1, "The Lord of the Rings", "J. R. R. Tolkien"), 7);
+		books.put(new Book(2, "Le Petit Prince", "Antoine de Saint-Exupery"), 5);
+		books.put(new Book(3, "Harry Potter and the Philosopher's Stone", "J. K. Rowling"), 0);
+		books.put(new Book(4, "And Then There Were None", "Agatha Christie"), 12);
+		books.put(new Book(5, "Dream of the Red Chamber", "Cao Xueqin"), 1);
+		books.put(new Book(6, "The Hobbit", "J. R. R. Tolkien"), 1);
+		books.put(new Book(7, "She: A History of Adventure", "H. Rider Haggard"), 1);
+		books.put(new Book(8, "The Lion, the Witch and the Wardrobe", "C. S. Lewis"), 3);
+		books.put(new Book(9, "The Da Vinci Code", "Dan Brown"), 4);
+		books.put(new Book(10, "The Catcher in the Rye", "J. D. Salinger"), 111);
+		books.put(new Book(11, "The Hobbit", "AB Svenska Ord"), 200);
 
 		System.out.println();
-		System.out.println("Unsorted Books, as stored in ArrayList.");
+		System.out.println("Unsorted Books, as stored in HashMap.");
 		System.out.println("=========================================");
-		for (Book nextBook : books) {
-			System.out.println(nextBook);
-		}
 
-		// Sort using defined Comparator
-		// provided TitleSort class
-		System.out.println();
-		System.out.println("Sorted books - using TitelSort class");
-		System.out.println("=================================================================");
-		Collections.sort(books, new TitleSort());
-
-		for (Book nextBook : books) {
-			System.out.println(nextBook);
-		}
-
-		System.out.println();
-		System.out.println("Sorted books - using own ReverseTitleComparator class");
-		System.out.println("=================================================================");
-		Collections.sort(books, new ReverseTitleComparator());
-		for (Book nextBook : books) {
-			System.out.println(nextBook);
-		}
-
-		System.out.println();
-		System.out.println("Sorted books - sort on id using comparator defined as anonymous inner class");
-		System.out.println("=================================================================");
-		Collections.sort(books, new Comparator<Book>() {
-
-			@Override
-			public int compare(Book o1, Book o2) {
-				return o1.getId() - o2.getId();
-			}
-
-		});
-
-		for (Book nextBook : books) {
-			System.out.println(nextBook);
-		}
-
+		Integer result= books.get(new Book(11, "The Hobbit", "AB Svenska Ord"));
+		System.out.println("result=" + result);
 	}
 
 }
